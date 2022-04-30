c2b288d350b1014cd9d247f95b1b8c68abfe0623
=Restu Averian Putra <=restuaverianputra123@gmail.com>
2022-04-30 14:43:38 +0700

    add logout-feature

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <template>
   <div id="app">
     <nav>
+      <div v-if="LoginOrNot == true">
+        <span @click="LogOut" class="btn btn-danger">Logout</span>
+        <p>Login as {{ dataLogin }}</p>
+      </div>
+      <div v-else>
+        <router-link to="/login">Login</router-link> |
+        <router-link to="/register">Register</router-link> |
+      </div>
       <router-link to="/">Home</router-link> |
       <router-link to="/cart">Cart</router-link>
     </nav>
     <router-view />
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      LoginOrNot: false,
+      dataLogin: "",
+    };
+  },
+  computed: {},
 
+  methods: {
+    userLogin() {
+      let user = localStorage.getItem("dataHp");
+      let dataUser = JSON.parse(user);
+      if (dataUser !== null) {
+        //Udh Login
+        this.dataLogin = dataUser.dataHp.UserLogin.username;
+        this.LoginOrNot = true;
+      } else {
+        // Blum Login
+        this.LoginOrNot = false;
+        console.log("it's null");
+      }
+      console.log(dataUser);
+    },
+    LogOut() {
+      let keluar = confirm("Yakin dek ? ");
+      if (keluar) {
+        localStorage.removeItem("dataHp");
+      } else {
+        alert("Bacot luwh dek");
+      }
+    },
+  },
+  mounted() {
+    this.userLogin();
+  },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
